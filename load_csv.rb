88b6e9466be4569e30cb005c4281cb7ad381aa2d
Chris Mytton <mail@chrismytton.com>
2022-12-19 16:58:47 +0000

    Tweak code

--- load_csv.rb
+++ load_csv.rb
@@ -42,14 +42,12 @@ csv.each do |row|
     next
   end
 
-  # Prepared statement
   sql = "INSERT INTO land_and_assets (Organisation, OrganisationLabel, UPRN, AssetCode, PropertyName, StreetName, PostTown, PostCode, GeoX, GeoY, TenureType, TenureDetail, HoldingType) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)"
-
-  warn "Inserting row #{row["AssetCode"]}"
 
   begin
     # Insert the row into the table
     conn.exec(sql, row.values_at("Organisation", "OrganisationLabel", "UPRN", "AssetCode", "PropertyName", "StreetName", "PostTown", "PostCode", "GeoX", "GeoY", "TenureType", "TenureDetail", "HoldingType"))
+    warn "Row #{row["AssetCode"]} inserted"
   rescue PG::UniqueViolation
     # If the row already exists, skip it
     warn "Row #{row["AssetCode"]} already exists"
