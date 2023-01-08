0c86b08b14da1cc28d89ca993f24b862d351849b
Chris Mytton <mail@chrismytton.com>
2023-01-08 21:50:24 +0000

    Consistent capitalisation

--- extract_csv_to_ogr.sql
+++ extract_csv_to_ogr.sql
@@ -1,8 +1,8 @@
-SELECT AssetCode as id,
-  UPRN as uprn,
-  Organisation as org,
-  OrganisationLabel as org_label,
-  PropertyName as property,
+SELECT AssetCode AS id,
+  UPRN AS uprn,
+  Organisation AS org,
+  OrganisationLabel AS org_label,
+  PropertyName AS property,
 
   -- StreetName has some carriage returns and line feeds in it, so we need to clean it up.
   TRIM(
@@ -11,32 +11,32 @@ SELECT AssetCode as id,
       CHAR(10),
       ' '
     )
-  ) as street,
+  ) AS street,
 
-  PostTown as town,
+  PostTown AS town,
 
   -- Postcode has some carriage returns and line feeds in it, so we need to clean it up.
   REPLACE(
     REPLACE(TRIM(Postcode), CHAR(13), ''),
     CHAR(10),
     ''
-  ) as postcode,
+  ) AS postcode,
 
   -- Clean up and combine the tenure columns.
   REPLACE(TenureType, CAST(X'a0' AS TEXT), '') || ' - ' || TRIM(TenureDetail) as tenure,
 
-  HoldingType as holding,
+  HoldingType AS holding,
 
   -- Convert the GeoX and GeoY columns to a geometry.
   Transform(
     SetSRID(
-      MakePoint(CAST(GeoX as INTEGER), CAST(GeoY AS INTEGER)),
+      MakePoint(CAST(GeoX AS INTEGER), CAST(GeoY AS INTEGER)),
       27700
     ),
     4326
-  ) as geom
+  ) AS geom
 
-from local_authority_land_and_assets_2021
+FROM local_authority_land_and_assets_2021
 
 -- Only include rows where we have a valid geometry.
 WHERE GeoX != '' AND GeoY != ''
